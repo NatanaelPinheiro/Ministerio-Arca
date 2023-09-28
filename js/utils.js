62b1a1533bec25e3307dc9c3103a85fe283cfe31
Natanael Pinheiro <natanael.pinheiro@futuraproducoes.com.br>
2023-09-28 14:48:31 -0300

    ajustando toggle nav bar

--- js/utils.js
+++ js/utils.js
@@ -9,3 +9,13 @@ navMobileLinks.forEach((link) => {
         navMobile.classList.remove('active');
     })
 });
+
+const body = document.querySelector('body');
+body.addEventListener('click', function(event){
+    if(event.target.classList.contains("navbar-mobile") || event.target.classList.contains("menu-burger"))
+        return;
+    if(event.target.closest(".navbar-mobile") || event.target.closest(".menu-burger"))
+        return;
+    if(document.querySelector(".menu-burger-close"))
+        navMobile.classList.remove('active');
+});
